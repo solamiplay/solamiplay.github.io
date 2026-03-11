44f88e721c18fc122ca00c761370d3719e6b6ebe
Bert Hoffmann <rezenzeffekt@gmail.com>
2026-03-11 09:47:06 +0100

    almost there

--- tmp/eurc.js
+++ tmp/eurc.js
@@ -10,19 +10,41 @@ function jumpP(x) {
     document.getElementById('pages').style.setProperty('top', x)
 }
 
-// check wallet python call function
-// display error or return wallet in variable & save to cookies + jumpP('-100%')  
+async function eurc(x) {
+    add = x
+    
+    const response = await fetch('/api/eurc', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ add: add })
+    });
+
+    const result = await response.json();
+    if (result.message === 'confirmed') {
+        document.getElementById('noti').setAttribute('class', '')
+        document.getElementById('noti').innerHTML=''
+        var shorty = add.slice(0,4)+'...'+add.slice(-4)
+        localStorage.setItem('eurc',add)
+        // draw shorty at second page
+        jumpP('-100%')
+        return shorty 
+    } else {
+        document.getElementById('noti').setAttribute('class', 'n1')
+        document.getElementById('noti').innerHTML=result.message
+    }
+}
 
 async function paste() {
     var add = await navigator.clipboard.readText()
     var trunc = clip(add, 150)
     document.getElementById('add').innerHTML=trunc
     if (add.length > 31 && add.length < 45 && !(/\s/.test(add))) {
-        // remove previous error/message
-        // show checking message
-        // return python call function
+        document.getElementById('noti').setAttribute('class', 'n2')
+        document.getElementById('noti').innerHTML='... checking ...'
+        // return eurc(add)
     } else {    
-        // show error
+        document.getElementById('noti').setAttribute('class', 'n1')
+        document.getElementById('noti').innerHTML='Not a valid Address'
     }
 }
 
@@ -33,3 +55,14 @@ var clk = function() {
 }
 
 document.getElementById('c01').onclick = clk
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (localStorage.getItem('eurc') !== null) {
+        var add = localStorage.getItem('eurc')
+        var shorty = add.slice(0,4)+'...'+add.slice(-4)
+        // draw shorty at second page
+        jumpP('-100%')
+        return shorty
+    } 
+})
+
